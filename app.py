e3e4ac9b01eb9534e5c9a8c8b18892ceb2c03389
Benjamin Santana <0211518@up.edu.mx>
2021-03-11 19:35:23 -0600

    sqlalchemy working just fine
    here we added the sql module, and created a simple model
    for a table, then we added an entry to the table, and printed.
    stopped the program, print again, to check for consistency,
    everything working fine
    
    Signed-off-by: Benjamin Santana <0211518@up.edu.mx>

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import flask
+import flask_sqlalchemy
 
 app = flask.Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' # db path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = flask_sqlalchemy.SQLAlchemy(app) # create db
+
+# database model
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    complete = db.Column(db.Boolean)
+
+# index
 @app.route('/')
 def index():
-    return "hello world!"
+    # show todos
+    todo_list = Todo.query.all() # returns list with all db
+    print(todo_list)
+    return flask.render_template('base.html')
 
-@app.route('/about')
-def about():
-    return "about me"
-    
 if __name__=="__main__":
+    db.create_all()
+    # add something to data base
     app.run(debug=True)
