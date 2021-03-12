a0d9d854b97b961a298b9f653ed71ebe5b020802
Benjamin Santana <0211518@up.edu.mx>
2021-03-11 19:09:57 -0600

    added about page

--- app.py
+++ app.py
@@ -4,4 +4,11 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-	return "hello world!"
+    return "hello world!"
+
+@app.route('/about')
+def about():
+    return "about me"
+    
+if __name__=="__main__":
+    app.run(debug=True)
